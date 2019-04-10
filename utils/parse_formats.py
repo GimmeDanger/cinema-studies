d92cfbbe2f6c34ecd024479038382600a2d62483
GimmeDanger <vladim1r.nazarov@yandex.ru>
2019-04-11 01:44:08 +0300

    NEW: deadline gathering: parse format

--- utils/parse_formats.py
+++ utils/parse_formats.py
@@ -8,8 +8,17 @@ class VarietyParseFormat:
     def needle_URL_regexp(self, cat_name):
         return f"^{self.host_name}/\d*/film/{cat_name}"
 
+class DeadlineParseFormat:
+    host_name = "https://deadline.com"
+    categories = []
+    min_page_num = 1
+    max_page_num = 100000 #inf
+    def hay_page_URL(self, page_num):
+        return f"{self.host_name}/v/film/page/{page_num}"
+    def needle_URL_regexp(self):
+        return f"^{self.host_name}/\d*/\d*/[-\w]*"
+
 #TODO:
-#Deadline ( https://deadline.com )
 #The Hollywood Reporter ( https://www.hollywoodreporter.com/ )
 #Entertainment Weekly ( https://ew.com/ )
 #Film Comment ( https://www.filmcomment.com/ )

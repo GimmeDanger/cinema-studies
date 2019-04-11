77614d2bbc1c7a241a1dcfd829b22e7328a56592
GimmeDanger <vladim1r.nazarov@yandex.ru>
2019-04-11 11:45:33 +0300

    FIX: parse_formats: indentions

--- utils/parse_formats.py
+++ utils/parse_formats.py
@@ -1,17 +1,17 @@
 class VarietyParseFormat:
-	name = "variety"
-    host_name = f"https://{self.name}.com"
+    name = "variety"
+    host_name = f"https://{name}.com"
     categories = ["news", "reviews", "columns"]
     min_page_num = 1
-    max_page_num = 100000 #inf    
+    max_page_num = 100000 #inf
     def hay_page_URL(self, cat_name, page_num):
         return f"{self.host_name}/v/film/{cat_name}/page/{page_num}"
     def needle_URL_regexp(self, cat_name):
         return f"^{self.host_name}/\d*/film/{cat_name}"
 
 class DeadlineParseFormat:
-	name = "deadline"
-    host_name = f"https://{self.name}.com"
+    name = "deadline"
+    host_name = f"https://{name}.com"
     categories = ["film"]
     min_page_num = 1
     max_page_num = 100000 #inf
@@ -24,4 +24,4 @@ class DeadlineParseFormat:
 #The Hollywood Reporter ( https://www.hollywoodreporter.com/ )
 #Entertainment Weekly ( https://ew.com/ )
 #Film Comment ( https://www.filmcomment.com/ )
-#BoxOffice ( https://www.boxofficepro.com )        
+#BoxOffice ( https://www.boxofficepro.com )
